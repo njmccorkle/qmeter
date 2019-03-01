64acf9ac3bc441fad92a7826169b68374e3921aa
Nathan McCorkle <njmccorkle@gmail.com>
2019-03-01 15:32:22 -0600

    Update environment script

--- qmeter/environment.ps1
+++ qmeter/environment.ps1
@@ -1,10 +1,10 @@
 $name = $env:COMPUTERNAME
 
-if ($name = "DESKTOP-DBH6673") {
+if ($name -eq "DESKTOP-DBH6673") {
 	$env:PATH = "c:\git\qmeter\qmeter\venv\Scripts"
 	$env:FLASK_APP = "C:\git\qmeter\qmeter\runserver.py"
 }
-elseif ($name = "WSXXX") {
+elseif ($name -eq "W0209066") {
 	$env:PATH = "c:\Users\a8n90zz\source\repos\njmccorkle\qmeter\qmeter\venv\Scripts"
-	$env:FLASK_APP = "c:\Users\a8n90zz\source\repos\njmccorkle\qmeter\qmeter\$appName"
+	$env:FLASK_APP = "c:\Users\a8n90zz\source\repos\njmccorkle\qmeter\qmeter\runserver.py"
 }
